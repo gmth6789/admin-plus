c9e6bc438ab3274724e7ba5f345b5755f7382204
Mook.gm99 <143313748+gmth6789@users.noreply.github.com>
2023-09-05 04:38:01 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-input placeholder="Please input" v-model="input"></el-input>
+  {{ input }}
+</template>
+
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const input = ref("");
+    return {
+      input,
+    };
+  },
+};
+</script>
